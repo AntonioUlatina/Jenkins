329c2b6cf4163635861575f5d4141344d17860fc
AntonioUlatina <antonio.alvarez1@ulatina.net>
2018-12-18 16:21:46 -0600

    First Hello

---
+++ src/main/java/com/mycompany/jenkins/Hello.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jenkins;
+
+/**
+ *
+ * @author Pavilion
+ */
+public class Hello {
+
+    private String name;
+    /**
+     * @param args the command line arguments
+     */
+
+    public Hello(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return "Hello, " + this.name;
+    }
+    
+    public static void main(String[] args) {
+        Hello antonio = new Hello("Antonio");
+        System.out.println(antonio);
+    }
+    
+}
+
+
